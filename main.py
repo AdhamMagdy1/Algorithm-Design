c7f880e8ed5fcb1a64a905860a7c9fd76265320c
hussienahmed859 <109195788+hussienahmed859@users.noreply.github.com>
2023-05-15 10:41:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import bisect
 
 # >>>>>>>>>>>>>>>>>>>Daynmic approach<<<<<<<<<<<<<<<<<<:
 def jobs_function(jobs):
-    #sort job list in descinding order
+    #sort job list in descinding order according to their profits
     jobs = sorted(jobs, key=lambda x: x[2], reverse=True)
     
     #declaring variables
@@ -20,9 +20,11 @@ def jobs_function(jobs):
     number_of_jobs = 0
     deadline = [False] * n
     
-    #iterate and find slot for the job
+    #iterates over each job and tries to find a slot for it such that its deadline is not missed
     for job in jobs:
         for i in range(min(n, job[1])-1, -1, -1):
+            #If a slot is found the job is assigned to that slot and the maximum profit and the number of jobs assigned are updated 
+            #If no slot is found the job is skipped
             if not deadline[i]:
                 deadline[i] = True
                 maximum_profit += job[2]
@@ -30,7 +32,7 @@ def jobs_function(jobs):
                 break
     #return results
     return number_of_jobs, maximum_profit
-  #apply input
+#apply input
 jobs = [(1,4,20),(2,1,10),(3,1,40),(4,1,30)]
 number_of_jobs, maximum_profit = jobs_function(jobs)
 print("Number of jobs done:", number_of_jobs)
